3c8dc507a3f6ee0a9ca981b50c21dbe4e8fd76c1
Justus Winter <justus@g10code.com>
2016-01-06 11:55:25 +0100

    tests/gpgscm: Add a TinySCHEME-based test driver.
    * configure.ac: Add new component.
    * tests/Makefile.am: Likewise.
    * tests/gpgscm/Makefile.am: New file.
    * tests/gpgscm/ffi-private.h: Likewise.
    * tests/gpgscm/ffi.c: Likewise.
    * tests/gpgscm/ffi.h: Likewise.
    * tests/gpgscm/ffi.scm: Likewise.
    * tests/gpgscm/lib.scm: Likewise.
    * tests/gpgscm/main.c: Likewise.
    * tests/gpgscm/private.h: Likewise.
    * tests/gpgscm/scheme-config.h: Likewise.
    * tests/gpgscm/t-child.c: Likewise.
    * tests/gpgscm/t-child.scm: Likewise.
    * tests/gpgscm/tests.scm: Likewise.
    
    Signed-off-by: Justus Winter <justus@g10code.com>

---
+++ tests/gpgscm/ffi.h
@@ -0,0 +1,29 @@
+/* FFI interface for TinySCHEME.
+ *
+ * Copyright (C) 2016 g10 code GmbH
+ *
+ * This file is part of GnuPG.
+ *
+ * GnuPG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GnuPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GPGSCM_FFI_H
+#define _GPGSCM_FFI_H
+
+#include "scheme.h"
+
+void ffi_init (scheme *sc, const char *argv0, int argc, const char **argv);
+void ffi_update (scheme *sc);
+
+#endif /* _GPGSCM_FFI_H */
